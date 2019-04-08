54e52850bad88570bd62aaf58acea0e28e70ad5d
shaharc1990 <shaharc1990@gmail.com>
2019-04-08 16:37:08 +0300

    updated af integration

--- app/src/main/java/com/example/androidtemplate/MainActivity.java
+++ app/src/main/java/com/example/androidtemplate/MainActivity.java
@@ -8,12 +8,19 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.appsflyer.AppsFlyerLib;
+
 public class MainActivity extends AppCompatActivity {
+    String AF_DEV_KEY = "AF_DEV_KEY_PLACEHOLDER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppsFlyerLib.getInstance().setDebugLog(true);
+        AppsFlyerLib.getInstance().init(AF_DEV_KEY, null, getApplicationContext());
+        AppsFlyerLib.getInstance().startTracking(getApplication());
 
         Button rotateBtn = (Button) findViewById(R.id.rotate_btn);
         TextView orientationTv = (TextView) findViewById(R.id.orientation_txt);
@@ -29,9 +36,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "changing rotation", Toast.LENGTH_LONG).show();
-                if(rotation == 0) {
+                if (rotation == 0) {
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-                }else{
+                } else {
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
                 }
             }
